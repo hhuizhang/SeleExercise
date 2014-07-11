2f5fa1c09d77e38128cdb3636abe529b1b1e145f
hhuizhang <cathyzh8807@gmail.com>
2014-07-11 15:34:57 +0800

    Add chapter6.WorkingwithWebDriver package.

--- SeleExercise/src/main/java/chapter6/WorkingwithWebDriver/TestChapter6.java
+++ SeleExercise/src/main/java/chapter6/WorkingwithWebDriver/TestChapter6.java
@@ -1,21 +1,41 @@
 package chapter6.WorkingwithWebDriver;
 
+import junit.framework.Assert;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
 public class TestChapter6 {
 
 	WebDriver driver;
 	
 	@Before
 	public void setUp(){
-		System.setProperty("webdriver.firefox.bin", "C:\\Users\\hzhang8\\AppData\\Local\\Mozilla Firefox\\firefox.exe");
-		driver = new FirefoxDriver();
+		//System.setProperty("webdriver.firefox.bin", "C:\\Users\\hzhang8\\AppData\\Local\\Mozilla Firefox\\firefox.exe");
+		/*driver = new FirefoxDriver();
 		driver.get("http://book.theautomatedtester.co.uk/chapter4");
+		FirefoxBinary binary = new FirefoxBinary();
+		binary.setEnvironmentProperty("webdriver.firefox.bin", "C:\\Users\\hzhang8\\AppData\\Local\\Mozilla Firefox\\firefox.exe");*/
+	/*	FirefoxProfile profile = new FirefoxProfile();
+		profile.setPreference("browser.startup.homepage","http://book.theautomatedtester.co.uk/chapter4");
+		driver = new FirefoxDriver(profile);*/
+		
+/*        System.setProperty("webdriver.chrome.driver", "C:\\01WorkDir\\chromedriver_win32\\chromedriver.exe");
+        ChromeOptions options = new ChromeOptions();
+        options.setBinary("C:\\Users\\hzhang8\\AppData\\Local\\Google\\Chrome\\Application\\chrome.exe");
+		driver = new ChromeDriver(options);*/
+		
+		//driver = new RemoteWebDriver("http://book.theautomatedtester.co.uk/chapter4",DesiredCapabilities.chrome());		
+		
+		System.setProperty("webdriver.ie.driver", "C:\\01WorkDir\\IEDriverServer.exe");
+		System.setProperty("webdriver.ie.dirver.logfile","C:\\01WorkDir\\IEdriverlog.txt");
+		driver = new InternetExplorerDriver();		
+		driver.get("http://book.theautomatedtester.co.uk/chapter4");
+		
 	}
 	
 	@After
@@ -25,12 +45,16 @@ public class TestChapter6 {
 	
 	@Test
 	public void testExample(){
-		WebElement element = driver.findElement(By.id("nextBid"));
-		element.sendKeys("100");
-		System.out.printf(element.getLocation().toString());
-		System.out.printf(element.getSize().toString());
-		System.out.printf(element.getTagName());
-		System.out.print(element.isDisplayed());
+		WebElement element1 = driver.findElement(By.id("nextBid"));
+		element1.sendKeys("100");
+		System.out.println(element1.getLocation().toString());
+		System.out.println(element1.getSize().toString());
+		System.out.println(element1.getTagName());
+		System.out.println(element1.isDisplayed());
+		WebElement element2 = driver.findElement(By.id("selectLoad"));
+		String value2 = element2.getAttribute("value");
+		Assert.assertEquals("Click to load the select below", value2);
+
 		
 	}
 }
